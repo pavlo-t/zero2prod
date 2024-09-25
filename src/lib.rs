9fa89df2f3cd595d3a6925d55a89fa63e1b1e8f0
Pavlo Tsapliienko <pavel.tsaplienko@gmail.com>
2024-09-25 21:22:32 +0300

    start test app on random port

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::net::TcpListener;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
@@ -5,11 +6,11 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
-    }).bind("127.0.0.1:8080")?.run();
+    }).listen(listener)?.run();
 
     Ok(server)
 }
