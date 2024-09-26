4d9c8500830a6ff0c18e8c4670d87d9888128862
Pavlo Tsapliienko <pavel.tsaplienko@gmail.com>
2024-09-26 22:32:57 +0300

    subscribe handler

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,18 @@
-use std::net::TcpListener;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::net::TcpListener;
 
 async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
     HttpResponse::Ok()
 }
 
@@ -10,6 +20,7 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     }).listen(listener)?.run();
 
     Ok(server)
