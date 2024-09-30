1f7b3f6ae446c689248a4856c1d5f90c2201658f
Pavlo Tsapliienko <pavel.tsaplienko@gmail.com>
2024-09-30 19:27:41 +0300

    add db connection to subscriptions (doesn't compile)

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,8 @@
 use actix_web::{web, HttpResponse, Responder};
+use chrono::Utc;
+use sqlx::types::{chrono, uuid};
+use sqlx::PgConnection;
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -6,6 +10,22 @@ pub struct FormData {
     name: String,
 }
 
-pub async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
-    HttpResponse::Ok()
+pub async fn subscribe(
+    form: web::Form<FormData>,
+    connection: web::Data<PgConnection>,
+) -> impl Responder {
+    sqlx::query!(
+        // language=SQL
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(connection.get_ref())
+    .await;
+    HttpResponse::Ok().finish()
 }
