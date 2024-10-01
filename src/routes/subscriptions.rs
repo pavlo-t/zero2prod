584d6f99820d354f015e35dbe30ab7c916922936
Pavlo Tsapliienko <pavel.tsaplienko@gmail.com>
2024-10-01 21:45:12 +0300

    instrumenting POST /subscriptions

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -11,6 +11,13 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> impl Responder {
+    let request_id = Uuid::new_v4();
+    log::info!(
+        "request_id {request_id} - Adding '{}' '{}' as a new subscriber.",
+        form.email,
+        form.name
+    );
+    log::info!("request_id {request_id} - Saving new subscriber details in the database");
     match sqlx::query!(
         // language=SQL
         r#"
@@ -25,9 +32,12 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> im
     .execute(pool.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            log::info!("request_id {request_id} - New subscriber details have been saved");
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {e}");
+            log::error!("request_id {request_id} - Failed to execute query: {e:?}");
             HttpResponse::InternalServerError().finish()
         }
     }
