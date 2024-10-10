b58a444653b07b44894c612002b06712742dd312
Pavlo Tsapliienko <pavel.tsaplienko@gmail.com>
2024-10-10 23:19:15 +0300

    enable ssl support in postgres container

---
+++ scripts/generate_certificates.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cd ./certificates || exit
+
+openssl req -new -text -passout pass:abcd -subj /CN=localhost -out server.req -keyout privkey.pem
+openssl rsa -in privkey.pem -passin pass:abcd -out server.key
+openssl req -x509 -in server.req -text -key server.key -out server.crt
+chmod 600 server.key
+sudo chown 999:999 server.key
+
+rm server.req privkey.pem
