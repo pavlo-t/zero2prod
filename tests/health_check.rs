e49cae8cae4e36c87d4ea0b0929f48525e0b13ad
Pavlo Tsapliienko <pavel.tsaplienko@gmail.com>
2024-10-12 14:26:23 +0300

    test invalid inputs

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -127,3 +127,31 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
         );
     }
 }
+
+#[tokio::test]
+async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
+    let address = spawn_app().await.address;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
+        ("name=Ursula&email=", "empty email"),
+        ("name=Ursula&email=definitely-not-an-email", "invalid email"),
+    ];
+
+    for (body, error_message) in test_cases {
+        let response = client
+            .post(format!("{address}/subscriptions"))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            200,
+            response.status().as_u16(),
+            "The API did not return 200 OK when the payload was {}.",
+            error_message
+        );
+    }
+}
