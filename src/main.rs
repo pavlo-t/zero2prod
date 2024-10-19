6c895b7b6afaa593b35b4c16cd8d632a0554c1d5
Pavlo Tsapliienko <pavel.tsaplienko@gmail.com>
2024-10-19 13:55:30 +0300

    adding email_client to app_data

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
+use zero2prod::email_client::EmailClient;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
@@ -10,11 +11,20 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
+
     let connection_pool = PgPool::connect_lazy_with(configuration.database.with_db());
+
+    let email_sender = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address.");
+    let email_client = EmailClient::new(configuration.email_client.base_url, email_sender);
+
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
     );
     let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+
+    run(listener, connection_pool, email_client)?.await
 }
