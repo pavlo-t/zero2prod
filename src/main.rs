f7d09a75e37d97bded9208cf9de591e2acfc6172
Pavlo Tsapliienko <pavel.tsaplienko@gmail.com>
2024-10-27 13:53:52 +0200

    refactoring: move server build logic from main to startup

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,14 @@
-use sqlx::PgPool;
-use std::net::TcpListener;
-use zero2prod::configuration::get_configuration;
-use zero2prod::email_client::EmailClient;
-use zero2prod::startup::run;
+use zero2prod::configuration::{get_configuration, Settings};
+use zero2prod::startup::build;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
-
+    
     let configuration = get_configuration().expect("Failed to read configuration.");
-
-    let connection_pool = PgPool::connect_lazy_with(configuration.database.with_db());
-
-    let email_sender = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address.");
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        email_sender,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
-
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-    let listener = TcpListener::bind(address)?;
-
-    run(listener, connection_pool, email_client)?.await
+    let server = build(configuration).await?;
+    server.await?;
+    Ok(())
 }
