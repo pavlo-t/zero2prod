234582dab3301e485adf187024eda230b92281f4
Pavlo Tsapliienko <pavel.tsaplienko@gmail.com>
2024-11-06 22:29:00 +0200

    improve error logging: error_chain_fmt function

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -106,7 +106,7 @@ impl std::fmt::Display for StoreTokenError {
 }
 impl std::fmt::Debug for StoreTokenError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}\nCaused by:\n\t{}", self, self.0)
+        error_chain_fmt(self, f)
     }
 }
 impl ResponseError for StoreTokenError {}
@@ -171,4 +171,17 @@ fn generate_subscription_token() -> String {
         .map(char::from)
         .take(25)
         .collect()
+}
+
+fn error_chain_fmt(
+    e: &impl std::error::Error,
+    f: &mut std::fmt::Formatter<'_>,
+) -> std::fmt::Result {
+    writeln!(f, "{}\n", e)?;
+    let mut current = e.source();
+    while let Some(cause) = current {
+        writeln!(f, "Caused by:\n\t{}", cause)?;
+        current = cause.source();
+    }
+    Ok(())
 }
