e530ad9cfddb37dbc28baea62ec6345edb40ba43
Pavlo Tsapliienko <pavel.tsaplienko@gmail.com>
2024-11-16 15:24:04 +0200

    add spans to measure performance of hashing

--- scripts/cargo_test_with_logs.sh
+++ scripts/cargo_test_with_logs.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 export RUST_LOG="sqlx=error,info"
-TEST_LOG=true cargo t subscribe_fails_if_there_is_a_fatal_database_error | bunyan
+
+#TEST_LOG=true cargo t subscribe_fails_if_there_is_a_fatal_database_error | bunyan
+TEST_LOG=true cargo test --quiet --release newsletters_are_delivered | grep "VERIFY PASSWORD" | bunyan
