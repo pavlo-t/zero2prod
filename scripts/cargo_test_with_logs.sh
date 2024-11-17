a0c3f99d6f80740e0625bc1b896bc53580901714
Pavlo Tsapliienko <pavel.tsaplienko@gmail.com>
2024-11-17 18:02:30 +0200

    protect from user enumeration attacks - same response time in different scenarios

--- scripts/cargo_test_with_logs.sh
+++ scripts/cargo_test_with_logs.sh
@@ -3,4 +3,8 @@
 export RUST_LOG="sqlx=error,info"
 
 #TEST_LOG=true cargo t subscribe_fails_if_there_is_a_fatal_database_error | bunyan
-TEST_LOG=true cargo test --quiet --release newsletters_are_delivered | grep "VERIFY PASSWORD" | bunyan
+#TEST_LOG=true cargo test --quiet --release newsletters_are_delivered | grep "VERIFY PASSWORD" | bunyan
+
+## Imitate user enumeration attack:
+#TEST_LOG=true cargo test --quiet --release non_existing_user_is_rejected | grep "HTTP REQUEST" | bunyan
+#TEST_LOG=true cargo test --quiet --release invalid_password_is_rejected | grep "HTTP REQUEST" | bunyan
