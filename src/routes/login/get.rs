1abcc70e52e9ce80ac43a0ec81f2773eda0a69db
Pavlo Tsapliienko <pavel.tsaplienko@gmail.com>
2024-11-23 22:55:04 +0200

    remove _flash cookie in GET /login

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,3 +1,4 @@
+use actix_web::cookie::Cookie;
 use actix_web::{http::header::ContentType, HttpRequest, HttpResponse};
 
 pub async fn login_form(request: HttpRequest) -> HttpResponse {
@@ -6,7 +7,7 @@ pub async fn login_form(request: HttpRequest) -> HttpResponse {
         Some(cookie) => format!("<p><i>{}</i></p>", cookie.value()),
     };
 
-    HttpResponse::Ok()
+    let mut response = HttpResponse::Ok()
         .content_type(ContentType::html())
         .body(format!(
             // language=HTML
@@ -37,5 +38,10 @@ pub async fn login_form(request: HttpRequest) -> HttpResponse {
     </form>
 </body>
 </html>"#,
-        ))
+        ));
+    response
+        .add_removal_cookie(&Cookie::new("_flash", ""))
+        .unwrap();
+
+    response
 }
